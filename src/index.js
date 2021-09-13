ce68ef376c56a0ff052c53112db1d2e9e2089358
Ziquan Miao <ziquanmiao@Ziquans-MacBook-Pro.local>
2021-09-13 13:50:01 -0400

    remove rokt assets

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { RoktContextProvider } from "./contexts/RoktContextProvider";
 
 ReactDOM.render(
   <React.StrictMode>
